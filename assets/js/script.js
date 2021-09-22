d5b145174c880ae92187f3bb48117f8275aa81c3
Jake <xxjakexx14@gmail.com>
2021-09-22 11:51:20 -0400

    added function to color code time blocks according to current time

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,49 @@ var loadSchedule = function () {
   }
 
   $.each(schedule, function (key, val) {
-    //console.log(key, val);
     $('#' + key + '-desc').val(val);
+  });
+
+  auditSchedule();
+};
+
+var auditSchedule = function () {
+  $(".hour").each(function (i, obj) {
+    var timeBlock = obj.textContent;
+    var convetTimeBlock = moment(timeBlock, "hA");
+    var id = convertNumberToId(parseInt(timeBlock));
+
+    if (moment().isAfter(convetTimeBlock.add(1, "hour"))) {
+      $("#" + id + "-desc").addClass("list-group-item-dark");
+    } else if (moment().isBefore(convetTimeBlock.subtract(1, "hour"))) {
+      $("#" + id + "-desc").addClass("list-group-item-success");
+    } else {
+      $("#" + id + "-desc").addClass("list-group-item-info");
+    }
   });
 };
 
+var convertNumberToId = function (number) {
+  var numberNames = [
+    "",
+    "onePM",
+    "twoPM",
+    "threePM",
+    "fourPM",
+    "fivePM",
+    "",
+    "",
+    "",
+    "nineAM",
+    "tenAM",
+    "elevenAM",
+    "twelvePM",
+  ];
+  return numberNames[number];
+};
+
 var saveSchedule = function (id) {
-  var text = $('#' + id + '-desc').val();
+  var text = $("#" + id + "-desc").val();
 
   schedule[id] = text;
   localStorage.setItem(getScheduleFormat, JSON.stringify(schedule));
