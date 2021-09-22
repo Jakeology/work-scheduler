3b2df2c5db8f254a9d28b3046922e61a7d934460
Jake <xxjakexx14@gmail.com>
2021-09-21 22:00:32 -0400

    added function to save info to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,17 @@
 var getCurrentDayFormat = moment().format("dddd, MMMM Do");
+var getScheduleFormat = moment().format("MM-DD-YYYY");
 
+var schedule = {};
+
+var saveSchedule = function (id) {
+  var text = $('#' + id + '-desc').val();
+  
+  schedule[id] = text;
+  localStorage.setItem(getScheduleFormat, JSON.stringify(schedule));
+};
 
 $("#currentDay").text(getCurrentDayFormat);
 
+$("button").on("click", function () {
+  saveSchedule(this.id);
+});
