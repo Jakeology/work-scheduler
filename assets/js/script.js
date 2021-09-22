fae7495f895a61b4435591503bc90348947440ea
Jake <xxjakexx14@gmail.com>
2021-09-21 22:11:38 -0400

    added function to load saved schedule

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
 var getCurrentDayFormat = moment().format("dddd, MMMM Do");
 var getScheduleFormat = moment().format("MM-DD-YYYY");
 
-var schedule = {};
+var schedule;
+
+var loadSchedule = function () {
+  schedule = JSON.parse(localStorage.getItem(getScheduleFormat));
+
+  if (!schedule) {
+    schedule = {};
+  }
+
+  $.each(schedule, function (key, val) {
+    //console.log(key, val);
+    $('#' + key + '-desc').val(val);
+  });
+};
 
 var saveSchedule = function (id) {
   var text = $('#' + id + '-desc').val();
-  
+
   schedule[id] = text;
   localStorage.setItem(getScheduleFormat, JSON.stringify(schedule));
 };
@@ -15,3 +28,5 @@ $("#currentDay").text(getCurrentDayFormat);
 $("button").on("click", function () {
   saveSchedule(this.id);
 });
+
+loadSchedule();
